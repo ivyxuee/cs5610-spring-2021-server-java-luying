4d7113264cd1b064e53e2ba505477a758d912ee3
ivyxue <xue.lu@husky.neu.edu>
2021-03-20 15:36:10 -0700

    update delete method

--- src/main/java/com/example/wbdvsp2102luyingserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp2102luyingserverjava/models/Widget.java
@@ -2,17 +2,97 @@ package com.example.wbdvsp2102luyingserverjava.models;
 
 public class Widget {
 
-  private Long id;
+  private String id;
+  private String name;
   private String topicId;
+  private Integer widgetOrder;
+  private String url;
+  private Integer width;
+  private Integer height;
+  private String cssClass;
+  private String style;
+  private String value;
   private String type;
   private Integer size;
   private String text;
 
-  public Long getId() {
+  public Widget(String id, String topicId, String type, Integer size, String text) {
+    this.id = id;
+    this.topicId = topicId;
+    this.type = type;
+    this.size = size;
+    this.text = text;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getWidgetOrder() {
+    return widgetOrder;
+  }
+
+  public void setWidgetOrder(Integer widgetOrder) {
+    this.widgetOrder = widgetOrder;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public Integer getWidth() {
+    return width;
+  }
+
+  public void setWidth(Integer width) {
+    this.width = width;
+  }
+
+  public Integer getHeight() {
+    return height;
+  }
+
+  public void setHeight(Integer height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public String getId() {
     return id;
   }
 
-  public void setId(Long id) {
+  public void setId(String id) {
     this.id = id;
   }
 
@@ -48,13 +128,6 @@ public class Widget {
     this.text = text;
   }
 
-  public Widget(Long id, String topicId, String type, Integer size, String text) {
-    this.id = id;
-    this.topicId = topicId;
-    this.type = type;
-    this.size = size;
-    this.text = text;
-  }
 
   public Widget() {
   }
--- src/main/java/com/example/wbdvsp2102luyingserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2102luyingserverjava/services/WidgetService.java
@@ -2,8 +2,8 @@ package com.example.wbdvsp2102luyingserverjava.services;
 
 import com.example.wbdvsp2102luyingserverjava.models.Widget;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +12,11 @@ public class WidgetService {
   private List<Widget> widgets = new ArrayList<Widget>();
 
   {
-    Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
-    Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
-    Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
-    Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
-    Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
+    Widget w1 = new Widget("123l", "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
+    Widget w2 = new Widget("234l", "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
+    Widget w3 = new Widget("345l", "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
+    Widget w4 = new Widget("456l", "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
+    Widget w5 = new Widget("567l", "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
 
     widgets.add(w1);
     widgets.add(w2);
@@ -27,7 +27,8 @@ public class WidgetService {
 
   public Widget createWidgetForTopic(String topicId, Widget widget) {
     widget.setTopicId(topicId);
-    widget.setId((new Date()).getTime());
+    UUID uuid = UUID.randomUUID();
+    widget.setId(uuid.toString());
     widgets.add(widget);
     return widget;
   }
@@ -55,7 +56,7 @@ public class WidgetService {
     return null;
   }
 
-  public Integer deleteWidget(Long id) {
+  public Integer deleteWidget(String id) {
     int index = -1;
     for (int i = 0; i < widgets.size(); i++) {
       if (widgets.get(i).getId().equals(id)) {
@@ -67,7 +68,7 @@ public class WidgetService {
     return -1;
   }
 
-  public Integer updateWidget(Long id, Widget widget) {
+  public Integer updateWidget(String id, Widget widget) {
     int index = -1;
     for (int i = 0; i < widgets.size(); i++) {
       if (widgets.get(i).getId().equals(id)) {
